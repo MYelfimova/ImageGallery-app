eb12350c906c13b70bd9eb9b7231721b5380a0c3
Maria Yelfimova <maria.yelfimova@gmail.com>
2020-08-02 20:26:59 +0300

    Initial Commit

---
+++ ImageGallery app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageGallery app
+//
+//  Created by Maria Yelfimova on 8/2/20.
+//  Copyright © 2020 Maria Yelfimova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
